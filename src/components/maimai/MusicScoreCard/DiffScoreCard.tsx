5c0b9fb25c394740913c3ee91a05c7ebecba5dbc
r0x5a <155115880+r0x5a@users.noreply.github.com>
2025-02-21 19:02:16 +0800

    [maimai/music-scores] Add UTAGE support

--- src/components/maimai/MusicScoreCard/DiffScoreCard.tsx
+++ src/components/maimai/MusicScoreCard/DiffScoreCard.tsx
@@ -3,15 +3,15 @@ import type { JSX } from 'solid-js';
 import { Show } from 'solid-js';
 
 import Badge, { dxs, toDXStar } from '../Badge';
-import type { MusicData, Score } from '../def';
-import { calcRating, lvlData } from '../def';
+import type { LevelData, Score } from '../def';
+import { calcRating, Level, lvlData } from '../def';
 
-export function DiffScoreCard(p: { music: MusicData; diff: number; score: Omit<Score, 'music'> | undefined }) {
+export function DiffScoreCard(p: { chart: LevelData; diff: Level; score: Omit<Score, 'music'> | undefined }) {
 	const centerClass = 'flex items-center justify-center';
 	const gridClass = 'w-full h-full rounded-10px flex items-center justify-center';
 	const d = () => {
 		if (!p.score) return null;
-		const dxsMax = 3 * p.music.levels[p.diff].notes.total;
+		const dxsMax = 3 * p.chart.notes.total;
 		const score = p.score.dxs;
 		const dxAcc = score / dxsMax;
 		const dxStar = toDXStar(dxAcc) + 1;
@@ -19,10 +19,11 @@ export function DiffScoreCard(p: { music: MusicData; diff: number; score: Omit<S
 		const dxsBorder = d > 0 ? 'WTF' : d === 0 ? 'MAX' : `${dxStar === 6 ? 'MAX' : `⭐${dxStar}`} ${d}`;
 		return { dxsMax, dxAcc, dxStar, dxsBorder };
 	};
+	const style = () => lvlData[p.diff];
 	const SmallBox = (_p: { class?: string; children?: JSX.Element }) => (
 		<div class={clsx(centerClass, _p.class)}>
-			<div class={clsx(gridClass, lvlData[p.diff].bgBadges)}>
-				<div class={clsx('text-center', lvlData[p.diff].fg)}>
+			<div class={clsx(gridClass, style().bgBadges)}>
+				<div class={clsx('text-center', style().fg)}>
 					{_p.children}
 				</div>
 			</div>
@@ -31,23 +32,23 @@ export function DiffScoreCard(p: { music: MusicData; diff: number; score: Omit<S
 	return (
 		<div class={clsx('mt-2 font-bold font-digit', centerClass)}>
 			<div class='h-20 w-100% flex justify-between'>
-				<div class={clsx('w-36 rounded-3 shadow-md', lvlData[p.diff].bg, centerClass)}>
-					<div class={clsx('text-center', lvlData[p.diff].fg)}>
-						<div class='font-size-5 font-text'>{lvlData[p.diff].name}</div>
+				<div class={clsx('w-36 rounded-3 shadow-md', style().bg, centerClass)}>
+					<div class={clsx('text-center', style().fg)}>
+						<div class='font-size-5 font-text'>{style().name}</div>
 						<div class='font-size-4.4 font-digit'>
-							<Show when={p.music.levels[p.diff]?.rating} fallback='&emsp;'>
-								{p.music.levels[p.diff]?.rating.toFixed(1)}
+							<Show when={p.chart.rating} fallback='&emsp;'>
+								{p.diff === Level.UTG ? `${p.chart.diff.name}?` : p.chart.rating.toFixed(1)}
 							</Show>
 						</div>
 					</div>
 				</div>
-				<div class={clsx('ml-6 py-2 px-3 rounded-3 shadow-md flex gap-3 w-192', lvlData[p.diff].bg, !!p.score || 'opacity-40')}>
+				<div class={clsx('ml-6 py-2 px-3 rounded-3 shadow-md flex gap-3 w-192', style().bg, !!p.score || 'opacity-40')}>
 					<Show when={p.score}>
 						<SmallBox class='w-36 font-size-6.5'>
 							{`${(p.score!.acc / 10000).toFixed(4)}%`}
 						</SmallBox>
 						<SmallBox class='w-16 font-size-6.5'>
-							{calcRating(p.music.levels[p.diff].rating, p.score!.acc)}
+							{p.diff === Level.UTG ? '-' : calcRating(p.chart.rating, p.score!.acc)}
 						</SmallBox>
 						<SmallBox class='w-44'>
 							<div class='flex flex-col'>
@@ -61,10 +62,10 @@ export function DiffScoreCard(p: { music: MusicData; diff: number; score: Omit<S
 						<div class='w-32 flex items-center'>
 							<Badge.RankL class='h-14 w-auto object-contain' type={p.score!.rank} />
 						</div>
-						<div class='w-12 flex items-center justify-center'>
+						<div class='mx--1 w-14 flex items-center justify-center'>
 							<Badge.Combo class='h-12 w-auto object-contain' type={p.score!.combo} />
 						</div>
-						<div class='w-12 flex items-center justify-center'>
+						<div class='mx--1 w-14 flex items-center justify-center'>
 							<Badge.Sync class='h-12 w-auto object-contain' type={p.score!.sync} />
 						</div>
 						<div class='w-16 flex items-center justify-center'>
--- src/components/maimai/MusicScoreCard/index.tsx
+++ src/components/maimai/MusicScoreCard/index.tsx
@@ -1,29 +1,36 @@
 import clsx from 'clsx';
-import { createEffect, createSignal, For, Show } from 'solid-js';
+import { createMemo, For, Show } from 'solid-js';
 
-import type { MusicData, Score } from '../def';
+import { Level, type MusicData, type Score, sumLevels } from '../def';
 import { MusicInfo } from '../MusicCard/MusicInfo';
 import { DiffScoreCard } from './DiffScoreCard';
 
 type ScoreType = Omit<Score, 'music'>;
 
 export function MusicScoreCard(p: { music: MusicData; scores: ScoreType[]; class?: string }) {
-	const [getLevelMap, setLevelMap] = createSignal(new Map<number, ScoreType>());
-	createEffect(() => {
+	const levelMap = createMemo(() => {
 		const levelMap = new Map<number, ScoreType>();
 		for (const score of p.scores) {
 			levelMap.set(score.level, score);
 		}
-		setLevelMap(levelMap);
+		return levelMap;
 	});
 	return (
 		<div class={clsx('font-text flex justify-center', p.class)}>
 			<div class='w-256 border-8 border-white/20 border-rounded-xl border-solid bg-white/40 p-4'>
 				<MusicInfo music={p.music} />
 
-				<div class='mt-30px'>
+				<div class='mt-8'>
 					<Show when={!p.music.utage}>
-						<For each={[0, 1, 2, 3, 4]}>{id => <DiffScoreCard music={p.music} diff={id} score={getLevelMap().get(id)} />}</For>
+						<For each={[0, 1, 2, 3, 4]}>
+							{id => <DiffScoreCard chart={p.music.levels[id]} diff={id} score={levelMap().get(id)} />}
+						</For>
+					</Show>
+					<Show when={p.music.utage?.dp === false}>
+						<DiffScoreCard chart={p.music.levels[0]} diff={Level.UTG} score={levelMap().get(0)} />
+					</Show>
+					<Show when={p.music.utage?.dp === true}>
+						<DiffScoreCard chart={sumLevels(p.music.levels[0], p.music.levels[1])} diff={Level.UTG} score={levelMap().get(0)} />
 					</Show>
 				</div>
 			</div>
